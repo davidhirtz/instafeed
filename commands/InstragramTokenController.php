b00c70452cde22fbbeee3c68cd3d6d97f76bbc6a
David Hirtz <david.hirtz@me.com>
2022-08-26 17:47:25 +0200

    Create InstragramTokenController.php

---
+++ commands/InstragramTokenController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\commands;
+
+use app\models\InstagramToken;
+use davidhirtz\yii2\datetime\Date;
+use yii\console\Controller;
+
+/**
+ * InstagramTokenController takes care of refreshing the Instagram tokens on time.
+ * @noinspection PhpUnused
+ */
+class InstagramTokenController extends Controller
+{
+    /**
+     * Refreshes all Instagram tokens which expire in less than a month.
+     * @noinspection PhpUnused
+     */
+    public function actionRefreshAll()
+    {
+        $query = InstagramToken::find()->where(['expires_at' => new Date('1 month')]);
+
+        /** @var InstagramToken $instagram */
+        foreach ($query->each() as $instagram) {
+            $instagram->refreshAccessToken()
+                ->update();
+        }
+    }
+}
